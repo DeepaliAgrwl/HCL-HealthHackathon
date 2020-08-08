0e41283d66dc428d02a4e8b5c3567aa49b35b279
nitheshkumar-hcl <63444465+nitheshkumar-hcl@users.noreply.github.com>
2020-08-08 11:19:50 +0530

    Add files via upload

---
+++ ui.apps/src/main/content/jcr_root/apps/hackathon/clientlibs/clientlib-site/js/utility.js
@@ -0,0 +1,48 @@
+const setUserCookie = function (cname, cvalue, exdays) {
+	let d = new Date();
+	d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+	const expires = "expires=" + d.toGMTString();
+	document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+
+
+const getUserCookie = function (cname) {
+	let name = cname + "=";
+	let decodedCookie = decodeURIComponent(document.cookie);
+	let ca = decodedCookie.split(';');
+	for (let i = 0; i < ca.length; i++) {
+		let c = ca[i].trim();
+		if (c.indexOf(name) === 0) {
+			return c.substring(name.length, c.length);
+		}
+	}
+	return "";
+}
+
+const checkUserCookie = function(cname) {
+if(getUserCookie("hcluser") != "") {
+return true;
+} else {
+return false;
+}
+}
+
+
+var oldXHR = window.XMLHttpRequest;
+var checkmode =$('.checkmode').attr('checkmode');
+function newXHR() {
+    var realXHR = new oldXHR();
+    if(checkmode !== 'edit'){
+        realXHR.addEventListener("readystatechange", function() {
+            if(realXHR.readyState==1){
+                loader(true);
+            }
+            if(realXHR.readyState==4){
+                loader(false);           
+            }
+        }, false);
+    }
+    return realXHR;
+}
+window.XMLHttpRequest = newXHR;
